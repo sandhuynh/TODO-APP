31336b9fd955f47407b3369fae61b5debc61e60c
sandhuynh <nhoxsand@gmail.com>
2021-11-16 23:05:53 +0700

    Up code Todo App

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Folder; // ★ この行を追記！
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
+use Illuminate\Support\Facades\Auth;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }   
+
+    public function create(CreateFolder $request) // ★ 引数の型を変更
+    {
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+
+        // インスタンスの状態をデータベースに書き込む
+        Auth::user()->folders()->save($folder);
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use App\Mail\ResetPassword; // ★ 追加
+use Illuminate\Support\Facades\Mail; // ★ 追加
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function folders()
+    {
+        return $this->hasMany('App\Models\Folder');
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        Mail::to($this)->send(new ResetPassword($token));
+    }
+}
